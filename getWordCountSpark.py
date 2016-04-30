d81ab49a4a4a595e5816f2eb2e047a901716617c
Eric <yangeric7@gmail.com>
2016-04-30 15:21:00 -0500

    started using wikipedia data already on hdfs and gets word count for specific words

--- getWordCountSpark.py
+++ getWordCountSpark.py
@@ -1,23 +1,47 @@
 from pyspark import SparkConf, SparkContext
-import wikipedia
 import sys
+import re
 
-word_file = 'hdfs:///user/yange1/words.txt'
-
-def getWikiData(spark, listOfNames):
-    importantWord = ['MVP','All-Star','championship','award']
-    
-    names = spark.textFile(listOfNames)
-    names = names.map(lambda line: getText(line)) \
-            .flatMap(lambda (name,text):[(name + ',' + word, 1) if word in importantWord for word in text]) \
+word_file = 'hdfs:///user/yange1/words'
+name_file = 'names.txt'
+MAX_WORDS = 10000
+'''
+This part of the code uses some of the homework 3 code with modifications to fit our data gathering
+'''
+def getWikiData(spark, listOfNames, wikiFile):
+    importantWords = ['MVP', 'time', 'championship', 'award']
+    names = spark.textFile(wikiFile)
+    names = names.map(lambda line: get_title_and_text(line)) \
+            .flatMap(lambda (name,text):[(name + ',' + word, 1) for word in text if word in importantWords and name in listOfNames]) \
             .reduceByKey(lambda x,y: x+y) \
             .saveAsTextFile(word_file)
 
-def getText(line):
-    page = wikipedia.page(line)
-    content = page.content
+def getListOfNames(namefile):
+    listOfNames = []
+    f = open(namefile)
+    for line in f:
+        name = line.rstrip()
+        listOfNames.append(name)
 
-    return (line,content)
+    return listOfNames
+
+def get_title_and_text(text):
+    return (get_title(text), get_text(text))
+
+def get_title(text):
+    title = '<title>'
+    title_end = '</title>'
+    start = text.index(title) + len(title)
+    end = text.index(title_end)
+    return text[start:end]
+
+def get_text(text):
+    text_tag = '<text xml:space="preserve">'
+    text_end = '</text>'
+    start = text.index(text_tag) + len(text_tag)
+    end = text.index(text_end)
+    text_block = text[start:end]
+    return re.sub(r"\W+", ' ', text_block).strip().split(' ')
 
 if __name__ == '__main__':
     conf = SparkConf()
@@ -31,4 +55,5 @@ if __name__ == '__main__':
     conf.set("spark.executor.memory","10g")
     conf.set("spark.driver.memory","10g")
     spark = SparkContext(conf=conf)
-    getWikiData(spark,sys.argv[2])
+    listOfNames = getListOfNames(name_file)
+    getWikiData(spark,listOfNames,sys.argv[2])
