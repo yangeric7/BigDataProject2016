e0bc290568908305540b0ae073d77bb81622e214
Eric <yangeric7@gmail.com>
2016-05-01 14:09:32 -0500

    added data for ML and updated decision tree with auc score

--- MachineLearning/decisionTree.py
+++ MachineLearning/decisionTree.py
@@ -2,14 +2,17 @@ import csv
 import sys
 from sklearn import tree
 from sklearn import cross_validation
-
+from sklearn import metrics
 def prediction(feat,label):
     x_train, x_test, y_train, y_test = cross_validation.train_test_split(feat, label, test_size = 0.25, random_state = 0)
-    for depth in range(1,14):
+    for depth in range(1,10):
         clf = tree.DecisionTreeClassifier(max_depth = depth)
         clf.fit(x_train,y_train)
+        predictions = clf.predict(x_test)
         accuracy = clf.score(x_test,y_test)
-        print depth,accuracy
+        auc = metrics.roc_auc_score(y_test,predictions)
+        print depth,accuracy,auc
+        
 
 def getValues(filename):
     featureIndex = [1,2,3,4,10,15,25,28,29,30,31,32,34]
